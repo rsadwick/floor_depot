f8f9ca8a40c1a32a442baf35d333017dd2eceeca
Ryan Sadwick <rsadwick@gmail.com>
2017-07-22 20:13:15 -0400

    fuck

--- website/website/admin.py
+++ website/website/admin.py
@@ -1,15 +1,15 @@
 from copy import deepcopy
 from django.contrib import admin
-from cartridge.shop.admin import ProductAdmin
-from cartridge.shop.models import Product
+#from cartridge.shop.admin import ProductAdmin
+#from cartridge.shop.models import Product
 
-product_fieldsets = deepcopy(ProductAdmin.fieldsets)
-product_fieldsets[0][1]["fields"].insert(-2, "weight")
-
-
-class ProductWeight(ProductAdmin):
-    fieldsets = product_fieldsets
+#product_fieldsets = deepcopy(ProductAdmin.fieldsets)
+#product_fieldsets[0][1]["fields"].insert(-2, "weight")
 
 
-admin.site.unregister(Product)
-admin.site.register(Product, ProductWeight)
+#class ProductWeight(ProductAdmin):
+#    fieldsets = product_fieldsets
+
+
+#admin.site.unregister(Product)
+#admin.site.register(Product, ProductWeight)
