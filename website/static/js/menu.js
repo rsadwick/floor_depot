9e57df6d52c0070362c8912e0e268a7c2988308f
Ryan Sadwick <rsadwick@gmail.com>
2017-12-03 21:39:46 -0500

    navigation and left navigation

--- website/static/js/menu.js
+++ website/static/js/menu.js
@@ -1,6 +1,6 @@
-$(function() {
-		$('#main-menu').smartmenus({
-			subMenusSubOffsetX: 1,
-			subMenusSubOffsetY: -8
-		});
-	});
+$(function () {
+    $('#main-menu').smartmenus({
+        subMenusSubOffsetX: 1,
+        subMenusSubOffsetY: -8
+    });
+});
