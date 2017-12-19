f893300311e252490f2640aa20b87d1e7362e8ab
Ryan Sadwick <rsadwick@gmail.com>
2017-12-19 15:59:59 -0500

    ssl pooop

--- website/website/settings.py
+++ website/website/settings.py
@@ -174,7 +174,6 @@ USE_MODELTRANSLATION = False
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['localhost']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -267,7 +266,7 @@ EMAIL_FAIL_SILENTLY = True
 ###################
 
 # Domains for public site
-ALLOWED_HOSTS = ["hardwoodfloordepot.com"]
+ALLOWED_HOSTS = ["hardwoodfloordepot.com", "www.hardwoodfloordepot.com", "159.203.176.67"]
 
 # These settings are used by the default fabfile.py provided.
 # Check fabfile.py for defaults.
